924b500cb6d98f3c9bf218d4b2f29d6c08ddbc4d
adria960 <adria60@gmail.com>
2024-04-18 19:03:44 +0200

    cicd direct build - file updated

--- TestFile.java
+++ TestFile.java
@@ -2,5 +2,7 @@ public class TestFile {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello PlayJava!");
+		System.out.println("Final build with Jenkins and git cicd!");
+		System.out.println("We did it!");
 	}
 }
